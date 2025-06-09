c05d2c36d8f63736b78de10829cf6e9b85be4f8f
thatben <thatbenbierens@gmail.com>
2025-06-09 10:29:01 +0200

    setup

---
+++ PrototypeA/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { main } from './src/main.js';
+
+// Start the CLI application
+main().catch(console.error); 
---
+++ PrototypeA/src/main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+export async function main() {
+  console.log("a");
+}
