490fa253f7a474bb69ad280458ea2a724addfa88
thatben <thatbenbierens@gmail.com>
2025-06-14 10:20:38 +0200

    wip

--- PrototypeA/src/constellations/column.js
+++ PrototypeA/src/constellations/column.js
@@ -1,15 +1,29 @@
+export const ColumnUpdateCheckResponse = {
+  Unknown: "unknown",
+  Discard: "discard",   // The update should be discarded.
+  Delay: "delay",       // The update may be applied later.
+  Accept: "accept"      // The update should be applied immediately.
+};
+
+const exampleHandler = {
+    checkUpdate: async (signer, newValue) => { return ColumnUpdateCheckResponse.Unknown; },
+    onValueChanged: async () => { }
+};
+
 export class Column {
-    constructor(core, header, permissionChecker) {
+    constructor(core, header, handler) {
         this._core = core;
         this._logger = core.logger.prefix("Column:"+header);
 
         this._header = header;
-        this._checker = permissionChecker;
+        this._handler = handler;
 
+        this._hash = null;
         this._value = null;
         this._utc = null;
         this._signature = null;
         this._signer = null;
+        this._delayedPacketAndHashAndSigner = null;
     }
 
     get value() {
@@ -25,6 +39,7 @@ export class Column {
     }
 
     processPacket = async(packet) => {
+        if (!this._handler) this._logger.errorAndThrow("processPacket: handler not set.");
         if (packet.header != this._header) return false;
 
         const hash = this._core.cryptoService.sha256(
@@ -36,27 +51,30 @@ export class Column {
             packet.signature
         );
 
-        // even if signer is unchanged, still check:
-        // permissions may have changed!
-        if (!this._checker(signer)) {
-            this._logger.trace("processPacket: Rejected by permission checker.");
-            return true;
-        }
-
-        if (this._utc && packet.signedData.utc <= this._utc) {
-            this._logger.trace("processPacket: Rejected, not newer than current. ")
-            return true;
-        }
-
-        this._logger.trace("processPacket: Update accepted.");
-        this._value = packet.signedData.payload;
-        this._utc = packet.signedData.utc;
-        this._signature = packet.signature;
-        this._signer = signer;
+        await this._processPacket(packet, hash, signer);
         return true;
     }
 
+    applyDelayedUpdate = async () => {
+        if (!this._delayedPacketAndHashAndSigner) {
+            this.logger.trace("applyDelayedUpdate: No delayed packet was set.");
+            return;
+        }
+
+        await this._processPacket(
+            this._delayedPacketAndHashAndSigner.packet,
+            this._delayedPacketAndHashAndSigner.hash,
+            this._delayedPacketAndHashAndSigner.signer
+        )
+        
+        this._delayedPacketAndHashAndSigner = null;
+    }
+
     getAsPacket = async () => {
+        if (!this._signature) this._logger.errorAndThrow("getAsPacket: signature not set.");
+        if (!this._utc) this._logger.errorAndThrow("getAsPacket: utc not set.");
+        if (!this._value) this._logger.errorAndThrow("getAsPacket: value not set.");
+
         return {
             header: this._header,
             signature: this._signature,
@@ -65,5 +83,61 @@ export class Column {
                 payload: this._value
             }
         };
+    }
+
+    _processPacket = async (packet, hash, signer) => {
+        if (this._hash && this._hash == hash) {
+            this._logger.trace("_processPacket: Ignored, hash unchanged.");
+            return;
+        }
+
+        // even if signer/signature is unchanged, still check:
+        // permissions may have changed!
+        const checkResponse = await this._handler.checkUpdate(signer, packet.signedData.payload);
+        if (!checkResponse || checkResponse == ColumnUpdateCheckResponse.Unknown) {
+            this._logger.errorAndThrow("_processPacket: Invalid response from columnHandler.checkUpdate.");
+        }
+        if (checkResponse == ColumnUpdateCheckResponse.Discard) {
+            this._logger.trace("_processPacket: Rejected by columnHandler.checkUpdate.");
+            return;
+        }
+        if (checkResponse == ColumnUpdateCheckResponse.Delay) {
+            this._logger.trace("_processPacket: Delayed by columnHandler.");
+            this._updateDelayedPacket(packet, hash, signer);
+            return;
+        }
+
+        if (this._utc && packet.signedData.utc <= this._utc) {
+            this._logger.trace("_processPacket: Rejected, not newer than current.")
+            return;
+        }
+
+        await this._applyUpdate(packet, hash, signer);
+    }
+
+    _applyUpdate = async (packet, hash, signer) => {
+        this._logger.trace("_applyUpdate: Update accepted.");
+        this._hash = hash;
+        this._value = packet.signedData.payload;
+        this._utc = packet.signedData.utc;
+        this._signature = packet.signature;
+        this._signer = signer;
+
+        await this._handler.onValueChanged();
+    }
+
+    _updateDelayedPacket = (packet, hash, signer) => {
+        if (
+            // No delayed packet was previously set,
+            !this._delayedPacketAndHashAndSigner || 
+            // Or, the previous delayed packet is older than this one.
+            packet.signedData.utc > this._delayedPacketAndHashAndSigner.packet.signedData.utc
+        ) {
+            this._delayedPacketAndHashAndSigner = {
+                packet: packet,
+                hash: hash,
+                signer: signer
+            };
+        }
     }
 }
