93c40f3abc6f99b925e6121bb98aac11f291d9c6
ThatBen <thatbenbierens@gmail.com>
2025-06-16 13:15:16 +0200

    codex and waku mocks. and twostar test

--- PrototypeA/tests/twoStar.test.js
+++ PrototypeA/tests/twoStar.test.js
@@ -1,4 +1,4 @@
-import { afterEach, beforeAll, beforeEach, describe, it } from "vitest";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { Logger } from "../src/services/logger";
 import { ConstellationNode } from "../src/constellations/constellationNode";
 import { Wallet } from "ethers";
@@ -7,6 +7,7 @@ import { WakuService } from "../src/services/wakuService";
 import { CodexService } from "../src/services/codexService";
 import { Core } from "../src/constellations/core";
 import { WakuNode } from "../src/services/wakuNode";
+import { MockCodexService, MockWakuService } from "./mocks";
 
 const codexAddress = "http://192.168.178.26:8081";
 
@@ -18,50 +19,79 @@ const wakuBootstrapNodes = [
 
 describe("TwoStarTest", () => {
     const logger = new Logger("TwoStarTest");
-    const codexService = new CodexService(logger, codexAddress);
-    const wakuNode = new WakuNode(logger, wakuBootstrapNodes);
+    const codexService = new MockCodexService();
+    const wakuService = new MockWakuService();
 
     function createCore(name) {
         const myLogger = logger.prefix(name);
         const wallet = Wallet.createRandom();
         const constellationNode = new ConstellationNode(wallet);
         const cryptoService = new CryptoService(constellationNode);
-        const wakuService = new WakuService(
-        myLogger,
-        wallet,
-        wakuNode,
-        );
-    
-        return new Core(
+
+        const core = new Core(
             myLogger,
             constellationNode,
             wakuService,
             codexService,
             cryptoService,
         );
+
+        codexService._core = core;
+        wakuService._core = core;
+
+        return core;
     }
 
     var core1 = {};
     var core2 = {};
-
-    beforeAll(async () => {
-        if (!await codexService.isOnline()) logger.errorAndThrow("Test requires a codex node.");
-        await wakuNode.start();
-    })
-
-    afterAll(async () => {
-        await wakuNode.stop();
-    });
 
     beforeEach(() =>{
         core1 = createCore("One");
         core2 = createCore("Two");
     });
 
+    async function createStar(core, type, handler) {
+        const owners = [core.constellationNode.address];
+        return await core.starFactory.createNewStar(
+            type,
+            owners,
+            handler,
+        );
+    }
+
+    async function connectStar(core, starId, handler) {
+        return await core.starFactory.connectToStar(starId, handler);
+    }
 
     it("runs", async () => {
-        console.log("a");
+        const pastData = "pastData";
+        const presentData = "presentData";
+        const futureData = "futureData";
 
+        const doNothingHandler = {
+            onDataChanged: async (star) => {}
+        };
+        const star1 = await createStar(core1, "test_star", doNothingHandler);
+        const starId = star1.starId;
 
+        // Set past and present data before second node connects.
+        await star1.setData(pastData);
+        await star1.setData(presentData);
+
+        var receivedData = [];
+        const receiveHandler = {
+            onDataChanged: async (star) => {
+                receivedData.push(await star.getData());
+            }
+        };
+        const star2 = await connectStar(core2, starId, receiveHandler);
+
+        // Set future data.
+        await star1.setData(futureData);
+
+        // star1 has received present and future data.
+        expect(receivedData.length).toBe(2);
+        expect(receivedData[0]).toEqual(presentData);
+        expect(receivedData[1]).toEqual(futureData);
     })
 });
