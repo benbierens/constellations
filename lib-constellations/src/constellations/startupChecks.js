be455943397570bc93d2b42d1d6073cc3ea943b2
thatben <thatbenbierens@gmail.com>
2025-07-11 11:15:27 +0200

    Fixes wakuservice access

--- lib-constellations/src/constellations/startupChecks.js
+++ lib-constellations/src/constellations/startupChecks.js
@@ -33,7 +33,7 @@ export class StartupChecks {
     };
 
     const contentTopic = "/constellations/0/startupchecks/json";
-    const channel = await wakuService.openChannel(contentTopic, handler);
+    const channel = await this._core.wakuService.openChannel(contentTopic, handler);
 
     const msg = `Check:${Math.random() * 9999999}:${new Date().toISOString()}`;
     await channel.send(msg);
