a743089eab8675d4c6e00b0ce9272d1a8af8f87b
thatben <thatbenbierens@gmail.com>
2025-08-07 18:57:40 +0200

    wip

--- liftoff/index.js
+++ liftoff/index.js
@@ -1,20 +1,25 @@
 import { AppData } from "./appdata.js";
 import { Config } from "./config.js";
 import { Logger } from "./logger.js";
+import { Startup } from "./startup.js";
 
 process.env["CONSTELLATIONS_ADDRESS"] = "placeholder";
 process.env["CODEX_ADDRESS"] = "placeholder";
 process.env["SOURCE_PATH"] = "placeholder";
 process.env["DATA_PATH"] = "placeholder";
 
-const logger = new Logger();
-logger.log("Initializing...");
+async function main() {
+  const logger = new Logger();
+  logger.log("Initializing...");
 
-const config = new Config(logger);
-const appData = new AppData(logger, config);
+  const config = new Config(logger);
+  const appData = new AppData(logger, config);
 
-// if no constellation ID, create new constellation and save it.
+  await Startup(logger, config, appData);
 
-// iterate source path, make sure it's all update-to-date in the constellation.
-// iterate the constellation, make sure it's all update-to-date in the source path.
-// repeat until stopped.
+  // iterate source path, make sure it's all update-to-date in the constellation.
+  // iterate the constellation, make sure it's all update-to-date in the source path.
+  // repeat until stopped.
+}
+
+main().catch(console.error); 
---
+++ liftoff/startup.js
@@ -0,0 +1,3 @@
+export async function Startup(logger, config, appdata) {
+  // if no constellation ID, create new constellation and save it.
+}
